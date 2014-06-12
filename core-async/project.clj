770426726cb945af35b9d75fd5b97cb93e07c11c
Olivier Lange <olange@petit-atelier.ch>
2014-06-12 19:33:24 +0200

    Ajouté dépendance à core.async

--- core-async/project.clj
+++ core-async/project.clj
@@ -1,9 +1,10 @@
 (defproject core-async "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Learning core.async and ClojureScript, following ClojureScript 101 (http://swannodette.github.io/2013/11/07/clojurescript-101/)"
+  :url "https://github.com/olange/learning-clojure/tree/master/core-async"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
