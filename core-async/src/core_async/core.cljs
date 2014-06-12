c239fdb435ff8ac7e6242ba47d0252487159adb2
Olivier Lange <olange@petit-atelier.ch>
2014-06-12 19:33:42 +0200

    Premiers pas du tutoriel

--- core-async/src/core_async/core.cljs
+++ core-async/src/core_async/core.cljs
@@ -1,5 +1,37 @@
-(ns core-async.core)
+(ns core-async.core
+	(:require-macros [cljs.core.async.macros :refer [go]])
+	(:require [goog.dom :as dom]
+						[goog.events :as events]
+						[cljs.core.async :refer [put! chan <!]])
+	(:import	[goog.net Jsonp]
+          	[goog Uri]))
+
+(def wiki-search-url
+  "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=")
 
 (enable-console-print!)
 
 (println "Hello world!")
+
+(.log js/console (dom/getElement "query"))
+
+(defn listen [el type]
+  (let [out (chan)]
+    (events/listen el type
+      (fn [e] (put! out e)))
+    out))
+
+(let [clicks (listen (dom/getElement "search") "click")]
+  (go (while true
+        (.log js/console (<! clicks)))))
+
+(defn jsonp [uri]
+  (let [out (chan)
+        req (Jsonp. (Uri. uri))]
+    (.send req nil (fn [res] (put! out res)))
+    out))
+
+(defn query-url [q]
+  (str wiki-search-url q))
+
+(go (.log js/console (<! (jsonp (query-url "cats")))))
