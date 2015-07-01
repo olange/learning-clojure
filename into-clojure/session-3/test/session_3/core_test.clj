fa35d436f59afcbab1d906a5ac32e6ca54dc06fb
Olivier Lange <olange@petit-atelier.ch>
2015-07-01 21:35:01 +0200

    Remplacé idiome (is ...) par (are ...) dans tests

--- into-clojure/session-3/test/session_3/core_test.clj
+++ into-clojure/session-3/test/session_3/core_test.clj
@@ -4,13 +4,13 @@
 
 (deftest fibo-test
   (testing "the first numbers of the Fibonnacci suite"
-    (is (= 0 (fibo 0)))
-    (is (= 1 (fibo 1)))
-    (is (= 1 (fibo 2)))
-    (is (= 2 (fibo 3)))
-    (is (= 3 (fibo 4)))
-    (is (= 5 (fibo 5)))
-    (is (= 8 (fibo 6)))))
+    (are [exp val] (= exp (fibo val))
+        1 1
+        1 2
+        2 3
+        3 4
+        5 5
+        8 6)))
 
 (deftest fibo-suite-test
   (testing "the first 11 numbers of the Fibonnacci suite"
