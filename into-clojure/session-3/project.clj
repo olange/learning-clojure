061a5db2137bc1ebf440f64f24d834659bb61e7c
Olivier Lange <olange@petit-atelier.ch>
2015-07-15 19:55:06 +0200

    Màj. descripteurs des projets

--- into-clojure/session-3/project.clj
+++ into-clojure/session-3/project.clj
@@ -1,6 +1,6 @@
 (defproject session-3 "0.1.0-SNAPSHOT"
-  :description "(into Clojure) Exercices de la session 3"
-  :url "http://example.com/FIXME"
+  :description "Exercices de la session 3 du meetup (into Clojure)"
+  :url "https://github.com/olange/learning-clojure/tree/master/into-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
--- into-clojure/session-4/project.clj
+++ into-clojure/session-4/project.clj
@@ -1,6 +1,6 @@
 (defproject session-4 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Exercices de la session 4 du meetup (into Clojure)"
+  :url "https://github.com/olange/learning-clojure/tree/master/into-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
