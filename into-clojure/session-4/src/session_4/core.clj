87615c92f44da97a0fd4bd28537c80ba86f96578
Olivier Lange <olange@petit-atelier.ch>
2015-07-15 20:30:47 +0200

    Implémentation vérification gagnants jeu et partie

--- into-clojure/session-4/src/session_4/core.clj
+++ into-clojure/session-4/src/session_4/core.clj
@@ -1,5 +1,20 @@
 (ns session-4.core
+  "Identification des gagnants d'un jeu ou d'une partie de tennis"
   (:gen-class))
+
+(defn set-winner
+  [[a b]]
+  (if (> a b)
+      :a
+      :b))
+
+(defn winner
+  [score]
+  (let [sets-won-by (frequencies
+                       (map set-winner score))]
+    (if (> (sets-won-by :a 0)
+           (sets-won-by :b 0))
+       :a :b)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- into-clojure/session-4/test/session_4/core_test.clj
+++ into-clojure/session-4/test/session_4/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [session-4.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest set-winner-is-a
+  (testing "Gagnant du jeu est A"
+    (is (= :a (set-winner [7 6])))))
+
+(deftest set-winner-is-b
+  (testing "Gagnant du jeu est B"
+    (is (= :b (set-winner [3 6])))))
+
+(deftest winner-is-a
+  (testing "Gagnant de la partie est A"
+    (is (= :a (winner [[6 4] [6 0] [6 2]])))))
+
+(deftest winner-is-b
+  (testing "Gagnant de la partie est B"
+    (is (= :b (winner [[0 6] [6 3] [3 6] [5 7]])))))
